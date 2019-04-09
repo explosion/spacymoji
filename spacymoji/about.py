1d8f528355cabaa8c6ffbc5ce8d402f3a82ab655
Ines Montani <ines@ines.io>
2019-04-09 10:45:50 +0200

    Increment version

--- spacymoji/about.py
+++ spacymoji/about.py
@@ -1,5 +1,5 @@
 __title__ = 'spacymoji'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __summary__ = 'spaCy pipeline component for adding emoji meta data to Doc, Token and Span objects.'
 __uri__ = 'https://spacy.io'
 __author__ = 'Ines Montani'
